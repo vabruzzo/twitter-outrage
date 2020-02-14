b900f7a0d8cdd92f367396b095c49b4304bdb36b
Vincent Abruzzo <vgabruzzo@gmail.com>
2020-02-14 11:21:18 -0500

    select tweets with lt 20 ratings

--- src/controller/TweetActions.ts
+++ src/controller/TweetActions.ts
@@ -1,11 +1,12 @@
 import { Context } from 'koa';
+import { LessThan } from 'typeorm';
 import { getRepository } from './utils';
 import sampleSize = require('lodash.samplesize');
 import { Tweet } from '../entity/Tweet';
 
 const getTweets = async (ctx: Context) => {
   const tweets = await getRepository(Tweet).find({
-    where: { active: true },
+    where: { active: true, ratings: LessThan(20) },
     take: 1000,
     cache: 3600000,
   });
